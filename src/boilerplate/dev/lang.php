6f187d23903554b7a1ded7abce6819b8b801df5e
GaalexxC <gary@devcu.com>
2019-05-04 14:45:00 -0400

    Create lang.php

---
+++ src/boilerplate/dev/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang = array(
+	'__app_boilerplate'	=> "IPS Boilerplate",
+	'menutab__boilerplate'	=> "IPS Boilerplate",
+	'menutab__boilerplate_icon'	=> "globe",
+    'frontnavigation_boilerplate'	=> "Boilerplate",
+    'menu__boilerplate_dashboard'	=> "Dashboard",
+	'menu__boilerplate_boilerplate'	=> "IPS Boilerplate",
+	'menu__boilerplate_dashboard_settings'	=> "Settings",
+	'menu__boilerplate_dashboard_overview'	=> "Overview",
+
+	'head_boilerplate_overview'	=> "Overview",
+	'head_boilerplate_settings'	=> "Settings",
+	'head_boilerplate_settings_custom'	=> "Customize",
+
+	'boilerplate_enable'	=> "Boilerplate Enabled",
+	'boilerplate_groups'	=> "Boilerplate Group Access",
+	'boilerplate_application_name'	=> "Application Name",
+
+	'loc_boilerplate_browsing'	=> "Browsing Boilerplate",
+
+);
