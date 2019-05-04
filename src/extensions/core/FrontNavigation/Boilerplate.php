78130f611461a3fe5edfe738e5dd9eaf40c736a4
GaalexxC <gary@devcu.com>
2019-05-04 16:25:08 -0400

    root

---
+++ src/extensions/core/FrontNavigation/Boilerplate.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       Boilerplate Front Navigation Extension
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4+
+ * @subpackage	Boilerplate
+ * @version     1.0.0
+ * @source      https://github.com/GaalexxC/IPS-Application-Boilerplate
+ * @Issue Trak  https://www.devcu.com/devcu-tracker/
+ * @Created     04 MAY 2019
+ * @Updated     04 MAY 2019
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+namespace IPS\boilerplate\extensions\core\FrontNavigation;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Front Navigation Extension: Frontpage
+ */
+class _Boilerplate extends \IPS\core\FrontNavigation\FrontNavigationAbstract
+{
+	/**
+	 * Get Type Title which will display in the AdminCP Menu Manager
+	 *
+	 * @return	string
+	 */
+	public static function typeTitle()
+	{
+		return \IPS\Member::loggedIn()->language()->addToStack('frontnavigation_boilerplate');
+	}
+		
+	/**
+	 * Can the currently logged in user access the content this item links to?
+	 *
+	 * @return	bool
+	 */
+	public function canAccessContent()
+	{
+		return \IPS\Member::loggedIn()->canAccessModule( \IPS\Application\Module::get( 'boilerplate', 'dashboard' ) );
+	}
+
+	/**
+	 * Get Title
+	 *
+	 * @return	string
+	 */
+
+	public function title()
+	{
+		return \IPS\Settings::i()->boilerplate_application_name;
+	}
+
+	/**
+	 * Get Link
+	 *
+	 * @return	\IPS\Http\Url
+	 */
+	public function link()
+	{
+		return \IPS\Http\Url::internal( "app=boilerplate&module=dashboard&controller=main", 'front', 'dashboard' );
+	}
+	
+	/**
+	 * Is Active?
+	 *
+	 * @return	bool
+	 */
+	public function active()
+	{
+		return !\IPS\core\FrontNavigation::$clubTabActive and \IPS\Dispatcher::i()->application->directory === 'boilerplate';
+	}
+}
---
+++ src/modules/admin/dashboard/overview.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       Boilerplate Overview Controller
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4+
+ * @subpackage	Boilerplate
+ * @version     1.0.0
+ * @source      https://github.com/GaalexxC/IPS-Application-Boilerplate
+ * @Issue Trak  https://www.devcu.com/devcu-tracker/
+ * @Created     04 MAY 2019
+ * @Updated     04 MAY 2019
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+namespace IPS\boilerplate\modules\admin\dashboard;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Overview
+ */
+class _overview extends \IPS\Dispatcher\Controller
+{
+	/**
+	 * Execute
+	 *
+	 * @return	void
+	 */
+	public function execute()
+	{
+		\IPS\Dispatcher::i()->checkAcpPermission( 'settings_manage' );
+		parent::execute();
+	}
+
+	protected function manage()
+	{
+		\IPS\Dispatcher::i()->checkAcpPermission( 'settings_manage' );
+
+		$view = $this->_manageOverview();
+
+		\IPS\Output::i()->title = \IPS\Member::loggedIn()->language()->addToStack('head_boilerplate_overview');
+		\IPS\Output::i()->output = $view;
+}
+	/**
+	 * Overview
+	 *
+	 * @return	void
+	 */
+	protected function _manageOverview()
+	{
+		
+		/* Display */
+		return \IPS\Theme::i()->getTemplate( 'dashboard', 'boilerplate', 'admin' )->overview();
+	}
+	
+}
