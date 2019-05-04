ecf95733305670c4c57d356afca1b173bc2ad693
GaalexxC <gary@devcu.com>
2019-05-04 13:46:41 -0400

    Application

---
+++ src/boilerplate/Application.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ *     Support this Project... Keep it free! Become an Open Source Patron
+ *                       https://www.patreon.com/devcu
+ *
+ * @brief       Boilerplate Application Class
+ * @author      Gary Cornell for devCU Software Open Source Projects
+ * @copyright   (c) <a href='https://www.devcu.com'>devCU Software Development</a>
+ * @license     GNU General Public License v3.0
+ * @package     Invision Community Suite 4.4+
+ * @subpackage	Boilerplate
+ * @version     1.0.0
+ * @source      https://github.com/GaalexxC/IPS-Application-Boilerplate
+ * @Issue Trak  https://www.devcu.com/devcu-tracker/
+ * @Created     04 MAY 2019
+ * @Updated     04 MAY 2019
+ *
+ *                    GNU General Public License v3.0
+ *    This program is free software: you can redistribute it and/or modify       
+ *    it under the terms of the GNU General Public License as published by       
+ *    the Free Software Foundation, either version 3 of the License, or          
+ *    (at your option) any later version.                                        
+ *                                                                               
+ *    This program is distributed in the hope that it will be useful,            
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of             
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *                                                                               
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+ 
+namespace IPS\boilerplate;
+
+/**
+ * IPS Boilerplate Application Class
+ */
+class _Application extends \IPS\Application
+{
+	/**
+	 * Init
+	 *
+	 * @return	void
+	 */
+	public function init()
+	{
+		/* If the viewing member cannot view the board (ex: guests must login first), then send a 404 Not Found header here, before the Login page shows in the dispatcher */
+		if ( !\IPS\Member::loggedIn()->group['g_view_board'] and ( \IPS\Request::i()->module == 'boilerplate' and \IPS\Request::i()->controller == 'main' and \IPS\Request::i()->do == 'rss' ) )
+		{
+			\IPS\Output::i()->error( 'node_error', '2D220/1', 404, '' );
+		}
+	}
+	/**
+	 * [Node] Get Icon for tree
+	 *
+	 * @note	Return the class for the icon (e.g. 'globe')
+	 * @return	string|null
+	 */
+	protected function get__icon()
+	{
+		return 'cogs';
+	}
+	
+	/**
+	 * Default front navigation
+	 *
+	 * @code
+	 	
+	 	// Each item...
+	 	array(
+			'key'		=> 'Example',		// The extension key
+			'app'		=> 'core',			// [Optional] The extension application. If ommitted, uses this application	
+			'config'	=> array(...),		// [Optional] The configuration for the menu item
+			'title'		=> 'SomeLangKey',	// [Optional] If provided, the value of this language key will be copied to menu_item_X
+			'children'	=> array(...),		// [Optional] Array of child menu items for this item. Each has the same format.
+		)
+	 	
+	 	return array(
+		 	'rootTabs' 		=> array(), // These go in the top row
+		 	'browseTabs'	=> array(),	// These go under the Browse tab on a new install or when restoring the default configuraiton; or in the top row if installing the app later (when the Browse tab may not exist)
+		 	'browseTabsEnd'	=> array(),	// These go under the Browse tab after all other items on a new install or when restoring the default configuraiton; or in the top row if installing the app later (when the Browse tab may not exist)
+		 	'activityTabs'	=> array(),	// These go under the Activity tab on a new install or when restoring the default configuraiton; or in the top row if installing the app later (when the Activity tab may not exist)
+		)
+	 * @endcode
+	 * @return array
+	 */
+	public function defaultFrontNavigation()
+	{
+		return array(
+			'rootTabs'		=> array(),
+			'browseTabs'	=> array( array( 'key' => 'Boilerplate' ) ),
+			'browseTabsEnd'	=> array(),
+			'activityTabs'	=> array()
+		);
+	}
+}
